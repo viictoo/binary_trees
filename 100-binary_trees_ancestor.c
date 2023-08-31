d0f15c9b8b5303be0455ace246a373b8d7bc7c2a
Victor Langat <provicml@gmail.com>
2023-08-31 08:49:40 +0300

    added 100

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,31 +1,45 @@
 #include "binary_trees.h"
-
+int binary_tree_is_ancestor(const binary_tree_t *ancestor,
+		const binary_tree_t *node);
 /**
-  * binary_trees_ancestor - a function that creates a binary tree node
-  * @second: pointer to a node
-  * @first:pointer to a node
-  * Return: pointer to the node or NULL on failure
-  */
-
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ * Return: Pointer to the lowest common ancestor, or NULL if not found.
+ */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-
-	if (!second || !first)
+	if (!first || !second)
 		return (NULL);
 
-	if (!second->parent || !first->parent)
-		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	if (first->parent->n == second->n)
-		return (first->parent);
-	else if (first->parent->n != second->n)
-		(binary_trees_ancestor(first->parent, second));
+	if (binary_tree_is_ancestor(first, second))
+		return ((binary_tree_t *)first);
 
-	if (first->n == second->parent->n)
-		return (second->parent);
-	else if (first->n != second->parent->n)
-		(binary_trees_ancestor(first, second->parent));
+	if (binary_tree_is_ancestor(second, first))
+		return ((binary_tree_t *)second);
 
-	return (NULL);
+	return (binary_trees_ancestor(first->parent, second->parent));
 }
+
+/**
+ * binary_tree_is_ancestor - Checks if a node is an ancestor of another node.
+ * @ancestor: The potential ancestor node.
+ * @node: The target node.
+ * Return: 1 if ancestor is an ancestor of node, 0 otherwise.
+ */
+int binary_tree_is_ancestor(const binary_tree_t *ancestor,
+		const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (ancestor == node)
+		return (1);
+
+	return (binary_tree_is_ancestor(ancestor, node->parent));
+}
+
